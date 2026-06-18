b8881abd7a76cc029d1608a16852386d22308f1e
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-06-19 03:39:47 +0500

    chore: intialise nextjs project

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: 'OmniLeads: The Autonomous CRM & Sales Intelligence Hub',
+  description: "This project is a B2B SaaS platform where sales teams manage leads, but with a massive twist: the system predicts which leads will convert using traditional Machine Learning, and employs a team of autonomous AI agents to research prospects and draft highly personalized, mathematically optimized sales pitches using your company's proprietary data.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
